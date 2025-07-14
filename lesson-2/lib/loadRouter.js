00300a3fefc19bcdc4254a737791fdfb74a40572
Ruslan <ruslan201010@gmail.com>
2025-07-14 15:59:11 +0300

    fix: dynamic routing (#8)

--- lesson-2/lib/loadRouter.js
+++ lesson-2/lib/loadRouter.js
@@ -1,18 +1,77 @@
-function getUrlParams(path) {
-  const routeParams = [];
-  const routePath = path.replace(/(\/[^/]+)\/(\d+)/g, (match, pathSlice, id) => {
-    routeParams.push(id);
-    return `${pathSlice}/[id]`;
+import {
+  findEntryInFolder,
+  getFullFilePath,
+  isFileExists,
+  isFolderExists,
+} from '../utils/path.js';
+import { RouteNotFoundError } from '../utils/error.js';
+
+const routesFolderPath = getFullFilePath('../routes');
+
+const getDynamicParam = async (path) => {
+  const entry = await findEntryInFolder(path, /^\[(\w+)\]$/g);
+
+  if (!entry) return null;
+
+  const name = entry.slice(1, -1);
+
+  return {
+    name,
+    path: entry,
+  };
+};
+
+const parseRouteUrl = async (url) => {
+  const routeSegments = url.split('/').filter(Boolean);
+
+  return routeSegments.reduce(async (segmentData, segment) => {
+    const { params, folderPath } = await segmentData;
+
+    const segmentPath = `${folderPath}/${segment}`;
+
+    const isFolder = await isFolderExists(segmentPath);
+
+    if (isFolder) {
+      return {
+        params,
+        folderPath: segmentPath,
+      };
+    }
+
+    const dynamicParam = await getDynamicParam(folderPath);
+
+    if (!dynamicParam) {
+      // We don't need to go further if folder or dynamic path of segment is not exists
+      throw new RouteNotFoundError(url);
+    }
+
+    params[dynamicParam.name] = segment;
+
+    return {
+      params,
+      folderPath: `${folderPath}/${dynamicParam.path}`,
+    };
+  }, {
+    params: {},
+    folderPath: routesFolderPath,
   });
-  return { routePath, routeParams };
-}
+};
 
 async function loadRouter(url) {
-  const { routePath, routeParams } = getUrlParams(url);
+  const { params, folderPath } = await parseRouteUrl(url);
 
-  const router = await import(`../routes${routePath}/route.js`);
+  const routeFilePath = `${folderPath}/route.js`;
 
-  return { router, routeParams };
+  if (!await isFileExists(routeFilePath)) {
+    throw new RouteNotFoundError(url);
+  }
+
+  const router = await import(routeFilePath);
+
+  return {
+    router,
+    routeParams: params,
+  };
 }
 
 export { loadRouter };
